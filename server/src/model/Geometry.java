6262e28485b0948d1ac1d591d2f29029b58c0689
John Wu <john.wu@mail.mcgill.ca>
2017-01-28 12:20:04 -0500

    added more objects

---
+++ server/src/model/Geometry.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by JohnWu on 2017-01-28.
+ */
+public class Geometry {
+
+    public Geometry() {
+
+    }
+}
--- server/src/model/Mural.java
+++ server/src/model/Mural.java
@@ -5,6 +5,10 @@ package model;
  */
 public class Mural {
 
+    private String type;
+    private Properties properties;
+    private Geometry geometry;
+
     public Mural() {
 
     }
