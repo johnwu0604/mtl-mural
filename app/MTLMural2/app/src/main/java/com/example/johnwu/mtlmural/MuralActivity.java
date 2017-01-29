d1adb84d1a3c3eb69845c0e325f3c3205b3ed212
Tiffany <tiffany.wang@mail.mcgill.ca>
2017-01-28 20:06:09 -0500

    Reverted manfifest

--- app/MTLMural2/app/src/main/java/com/example/johnwu/mtlmural/MuralActivity.java
+++ app/MTLMural2/app/src/main/java/com/example/johnwu/mtlmural/MuralActivity.java
@@ -5,14 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.text.method.ScrollingMovementMethod;
-import android.widget.Toolbar;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MuralActivity extends FragmentActivity {
 
